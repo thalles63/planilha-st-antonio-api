d52abd192c6c3ce21101ad11ab4fd289bb9df745
Thalles Leonardelli <leonardellit@delagelanden.com>
2023-12-27 20:40:06 -0300

    Create api endpoints

--- index.js
+++ index.js
@@ -1,72 +1,62 @@
-const express = require('express')
-const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const express = require('express');
+const app = express();
+const db = require('@cyclic.sh/dynamodb');
+const { v4: uuidv4 } = require('uuid');
+;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const collection = db.collection("clientes");
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
+// Create an item
+app.post('/clientes', async (req, res) => {
+    const key = uuidv4();
+    const item = await collection.set(key, req.body);
+    res.send(item);
+})
 
-// Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
-
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
+// Update an item
+app.put('/clientes/:id', async (req, res) => {
+    const key = req.params.id;
+    const item = await collection.set(key, req.body);
+    res.send(item);
 })
 
 // Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
+app.delete('/clientes/:id', async (req, res) => {
+    const id = req.params.id;
+    const item = await collection.delete(id);
+    res.send(item);
 })
 
 // Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
+app.get('/clientes/:id', async (req, res) => {
+    const { props: clientesData } = await collection.get(req.params.id);
+    
+    res.send(clientesData);
 })
 
 // Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
+app.get('/clientes', async (req, res) => {
+    const { results: clientesData } = await collection.list();
+
+    const clientes = await Promise.all(
+        clientesData.map(async ({ key }) => { 
+            const result = (await collection.get(key)).props;
+            return { id: key, ...result };
+        })
+    );
+
+    res.send(clientes);
 })
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
+    res.send({ msg: 'no route handler found' });
 })
 
 // Start the server
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
+    console.log(`index.js listening on ${port}`);
 })
